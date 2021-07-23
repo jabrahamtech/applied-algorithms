ec151c815f1ae16cdd66e46c6901021eac140962
Jonathan Abraham <jabrahamtech@gmail.com>
2021-07-23 20:31:13 +1200

    fix bugs to run c++ input

--- program.cpp
+++ program.cpp
@@ -1,11 +1,52 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
 
 int main(){
+
+     /* 
+     Take in the first four lines of code, discarding the initial lines.
+     
+    string one, two, n, four;
+    getline(cin, one);
+    getline(cin, two);
+    getline(cin, n);
+    getline(cin, four);
+
+     /* 
+     Creates vector with vectors where the index is matched to the person as the zero index is n.
+     
+
+    vector< vector<int> > people;
+    int _n = n[-1];
+    cout << _n;
+    
+    string line;
+    while(getline(cin, line)){
+        cout << line << endl;
+    }
+    */
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     return 0;
 }
