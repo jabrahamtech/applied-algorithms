e4f5b886fb374ccdbcda78b885f3311bb6bf7b84
Jonathan Abraham <jabrahamtech@gmail.com>
2021-07-23 21:28:25 +1200

    created array of arrays and started input

--- program.cpp
+++ program.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <map>
 #include <string>
 #include <vector>
 
@@ -17,14 +16,22 @@ int main(){
     
       */
     string line;
-    vector< vector<int> > people;
+    int person = 1;
     while(getline(cin, line)){
         if (line[0] == 'n'){
             line.erase(0,3);
             int _n = stoi(line);
+            int people[2 * _n][_n];
+
+
 
         if(line[1] = ':'){
-            
+            line.erase(0,2);
+            for(int i; i < line.length(); i++){
+                if (i%2-1 == 0){
+                    
+                }
+            }
         }
         
         }
