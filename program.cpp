e08a2dc7e82463da3d7978f1518d86a4843ba9b6
Jonathan Abraham <jabrahamtech@gmail.com>
2021-07-26 12:07:36 +1200

    change logic ( vector of vectors)

--- program.cpp
+++ program.cpp
@@ -1,13 +1,44 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-int* string_to_array(int arr[], string str){
-    
-
+vector<int> string_to_array(string str){
+    vector<int> split_str;
+    int j = 0, i, k = 0;
+    for (int i = 0; i < str.size(); i++) {
+        //cout << str[i] << endl;
+        //cout << "test" <<endl;
+      if (str[i] == ' ') {
+          //cout << i << endl;
+          //cout << str.substr(j,i) << endl;
+          int n = stoi(str.substr(j,i));
+          split_str.push_back(n);
+          // cout << arr[k] << endl;
+          j = i;
+          k +=1;
+          
+      }
+      if(i == str.size()-1){
+        int n = stoi(str.substr(j));
+        split_str.push_back(n);
+        //cout << arr[k] << endl;
+      }
+    }
+    return split_str;
 }
 
+
+void print_vector(const std::vector<int> numbers){
+    for(int x = 0 ; x < numbers.size() ; x++){
+        if(x == numbers.size() - 1){
+            cout << numbers[x] << endl;
+        }else{
+            cout << numbers[x] << ' ';
+        }
+    }
+}
 
 int main(){
 
@@ -23,10 +54,15 @@ int main(){
     string line;
     int person = 1;
     int _n;
+    vector<vector<int> > single_list;
+    vector<int> n;
     while(getline(cin, line)){
         if (line[0] == 'n'){
-            line.erase(0,3);
-            _n = stoi(line);  
+            line.erase(0,3); 
+            //cout << "test" << endl;
+            _n = stoi(line); 
+            n.push_back(_n);
+            single_list.push_back(n);
             break;  
         }
     }
@@ -37,16 +73,20 @@ int main(){
     Split line into array and then add array to array of arrays.
 
     */   
+    vector<int> spl_str;
 
-    int people[2*_n][_n]; //created outside of IF statement
     while(getline(cin, line)){
         if(line[1] == ':'){
-            line.erase(0,2);
-            int pref_list[_n];
-            string_to_array(pref_list, line);
-
+            line.erase(0,3);
+            spl_str = string_to_array(line);
         }
+    print_vector(spl_str);
     }
 
     return 0;
 }
+
+
+
+
+
